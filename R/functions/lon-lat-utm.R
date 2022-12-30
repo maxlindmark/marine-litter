ee50f235218f6b49801ea8bfdaa6f757cdc071d6
Max Lindmark <max.lindmark@slu.se>
2022-12-30 09:18:43 +0100

    first commit

---
+++ R/functions/lon-lat-utm.R
@@ -0,0 +1,9 @@
+# Lon lat to UTM function
+# https://pbs-assess.github.io/sdmTMB/articles/pretty-plots.html
+LongLatToUTM <- function(x, y, zone){
+  xy <- data.frame(ID = 1:length(x), X = x, Y = y)
+  coordinates(xy) <- c("X", "Y")
+  proj4string(xy) <- CRS("+proj=longlat +datum=WGS84")  ## for example
+  res <- spTransform(xy, CRS(paste("+proj=utm +zone=",zone," ellps=WGS84",sep='')))
+  return(as.data.frame(res))
+}
